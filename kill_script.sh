2d2598fffe524e0e93607ba2cdcccd7bfe77df13
Chirag Shah <shahchirag1709@gmail.com>
2018-06-06 19:02:06 +0530

    Moved kill script to root

--- kill_script.sh
+++ kill_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ $(tail -1000 crawler.log | grep SUCCESS | wc -l) -eq 0 ]
+if [ $(tail -5000 crawler.log | grep SUCCESS | wc -l) -eq 0 ]
 then
   echo "Puppeteer instances stuck"
   for VARIABLE in $(ps aux | grep 'puppeteer' | awk '{print $2}')
